3ad5a9d648a8a12601dcc1de66f4cc9314894b9f
Leon Garcia <leon@wizeline.com>
2019-05-17 20:09:56 -0500

    Add namespace and improve destroy and add monitor

--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,7 @@
-kubectl delete all --all
-kubectl delete pvc --all
-kubectl delete secret --all
-kubectl delete svc --all
-kubectl delete configmap --all
+kubectl="kubectl --namespace postgres"
+$kubectl delete all --all
+$kubectl delete pvc --all
+$kubectl delete secret --all
+$kubectl delete svc --all
+$kubectl delete configmap --all
+kubectl delete namespace postgres
